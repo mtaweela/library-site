98389ff2d83bbef547fae1eb638d1b6d6481505d
mtaweela <mohamed.taweela56@gmail.com>
2018-02-12 18:36:10 +0200

    adding some models

--- kotobjy/views.py
+++ kotobjy/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.models import Permission, User
 
-# Create your views here.
+def auth(request):
+    # Create user and save to the database
+    user = User.objects.create_user('myusername', 'myemail@crazymail.com', 'mypassword')
+
+    # Update fields and then save again
+    user.first_name = 'John'
+    user.last_name = 'Citizen'
+    user.save()
