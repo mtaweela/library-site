14596383495248472e5af854b04c5986e2ede384
AAhmed Awad <abdelrhman.ahmed92@gmail.com>
2018-02-12 18:36:27 +0200

    Model added

--- kotobjy/models.py
+++ kotobjy/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+# Follow table used to track the authors from users
+class Follow(models.Model):
+    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    author_id = models.ForeignKey('author', on_delete=models.CASCADE,)
+
+
+# books that readed by user
+class User_books(models.Model):
+    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    book_id = models.ForeignKey('book', on_delete=models.CASCADE,)
+    rate = models.DecimalField(max_digits=1, decimal_places=1, default=0)
+    review = models.TextField(max_length=200,)
+
+
+# user wishlist
+class User_wish_list(models.Model):
+    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    book_id = models.ForeignKey('book', on_delete=models.CASCADE,)
+
+
+# books owned by authors
+class Author_books:
+    book_id = models.ForeignKey('book', on_delete=models.CASCADE,)
+    author_id = models.ForeignKey('author', on_delete=models.CASCADE,)
