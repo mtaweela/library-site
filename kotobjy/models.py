69bac96568a64fa1778b47556d76f07b5bd7e339
mtaweela <mohamed.taweela56@gmail.com>
2018-02-12 19:23:45 +0200

    fix models migration error

--- kotobjy/models.py
+++ kotobjy/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-
-
-# Create your models here.
-
+from django.contrib.auth.models import User
 
 # Follow table used to track the authors from users
 class Follow(models.Model):
-    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     author_id = models.ForeignKey('author', on_delete=models.CASCADE,)
 
 
 # books that readed by user
 class User_books(models.Model):
-    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     book_id = models.ForeignKey('book', on_delete=models.CASCADE,)
     rate = models.DecimalField(max_digits=1, decimal_places=1, default=0)
     review = models.TextField(max_length=200,)
@@ -20,7 +17,7 @@ class User_books(models.Model):
 
 # user wishlist
 class User_wish_list(models.Model):
-    user_id = models.ForeignKey('auth_user', on_delete=models.CASCADE,)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     book_id = models.ForeignKey('book', on_delete=models.CASCADE,)
 
 
@@ -30,25 +27,25 @@ class Author_books:
     author_id = models.ForeignKey('author', on_delete=models.CASCADE,)
 
 class Author(models.Model):
-    name = models.CharField(max_length=200, null=Flase, required=True)
+    name = models.CharField(max_length=200, null=False)
     pic = models.CharField(max_length=300)
     bio = models.CharField(max_length=2000)
 
 class Book(models.Model):
-    title = models.CharField(max_length=200, null=Flase, required=True)
-    pic = models.CharField(max_length=300, null=Flase)
+    title = models.CharField(max_length=200, null=False)
+    pic = models.CharField(max_length=300)
     describtion = models.CharField(max_length=2000)
-    category = models.CharField(max_length=200, null=Flase)
+    category = models.CharField(max_length=200, null=False)
     pub_date = models.DateTimeField('date published')
 
 class Ex_user(models.Model):
-    user_id = models.OneToOneField('auth_user', on_delete=models.CASCADE, primary_key=True, null=Flase)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     pic = models.CharField(max_length=300)
 
 
 class User_fav(models.Model):
-    user_id = models.ForeignKey('auth_user',on_delete=models.CASCADE)
-    cat_name = models.CharField(max_length=200, null=Flase)
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE)
+    cat_name = models.CharField(max_length=200, null=False)
 
     class Meta:
         unique_together = (("user_id","cat_name"))
--- library/settings.py
+++ library/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'kotobjy.apps.PollsConfig',
+    'kotobjy.apps.KotobjyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
