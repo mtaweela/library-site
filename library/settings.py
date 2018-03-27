ca1ba11fe4261d72e6cddb2e6f7d86332aba74af
mtaweela <mohamed.taweela56@gmail.com>
2018-03-27 14:38:16 +0200

    fix image bug

--- library/settings.py
+++ library/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -124,5 +124,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'kotobjy/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "/kotobjy/static"),
+)
